2f7a308548ad078f983f46573bddef7cd2e69fad
Miguel Fernandez Carratala <miguelfdezc99@gmail.com>
2021-06-15 22:19:56 +0200

    Métodos de servicio createUsuario y checkAdmin

--- services/autenticacion.service.js
+++ services/autenticacion.service.js
@@ -1,38 +1,61 @@
 'use strict';
 
-const admin = require('./firebase.service');
+const { admin, db } = require('./firebase.service');
 
 const service = {
-  createUser: async function ({
-    email,
-    emailVerified,
-    phoneNumber,
-    password,
-    displayName,
-    photoURL,
-    disabled,
-  }) {
+  checkAdmin: async function (uid) {
     try {
-      const user = await admin.auth().createUser({
-        email,
-        emailVerified,
-        phoneNumber,
-        password,
-        displayName,
-        photoURL,
-        disabled,
-      });
-
+      const user = await admin.auth().getUser(uid);
+      return user.customClaims.admin;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  },
+  createUsuario: async function ({ usuario, email, password }, collection) {
+    let user = null,
+      userDB = null;
+    try {
+      user = await admin
+        .auth()
+        .createUser({
+          email,
+          password,
+          photoURL: 'https://image.flaticon.com/icons/png/512/848/848043.png',
+        });
       await admin.auth().setCustomUserClaims(user.uid, {
-        admin: true,
-        cocheFavorito: 'Lamborghini Aventador GT 360 Plus Pro',
+        admin: collection === 'administradores',
       });
-
       const userWithClaims = await admin.auth().getUser(user.uid);
 
-      return userWithClaims;
+      const userRef = db.collection(collection).doc(user.uid);
+      if (collection === 'administradores') {
+        userDB = { usuario };
+      } else if (collection === 'musicos') {
+        userDB = {
+          usuario,
+          descripcion: '',
+          imagenFondo: '',
+          actuaciones: 0,
+          valoracion: 0.0,
+          fans: 0,
+        };
+      } else if (collection === 'negocios') {
+        userDB = {
+          usuario,
+          descripcion: '',
+          ubicacion: '',
+          imagenFondo: '',
+        };
+      }
+      await userRef.set(userDB);
+      userDB = await userRef.get();
+
+      return { ...userWithClaims, ...userDB.data() };
     } catch (err) {
-      throw new Error('Error al crear el usuario');
+      await admin.auth().deleteUser(user.uid);
+      console.error(err);
+      throw new Error(`Error al crear el usuario: ${err}`);
     }
   },
 };
