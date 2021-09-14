e4593cfbbf4906285a60f5675ddcabfe86d83f8b
Miguel Fernandez Carratala <miguelfdezc99@gmail.com>
2021-09-14 18:41:22 +0200

    Terminado CRUD bandas

--- routes/index.route.js
+++ routes/index.route.js
@@ -8,11 +8,13 @@ const authRouter = require('./auth.route');
 const userRouter = require('./user.route');
 const postRouter = require('./post.route');
 const eventRouter = require('./event.route');
+const bandRouter = require('./band.route');
 
 // rutas
 router.use(authRouter);
 router.use(userRouter);
 router.use(postRouter);
 router.use(eventRouter);
+router.use(bandRouter);
 
 module.exports = router;
